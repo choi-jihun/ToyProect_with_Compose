49f2ffa697c317ac1398586235db503a87860ce9
choi-jihun <102225856+choi-jihun@users.noreply.github.com>
2023-11-23 00:28:59 +0900

    FeedItem 수정

--- app/src/main/java/com/example/toyproject/ui/components/feed/FeedItem.kt
+++ app/src/main/java/com/example/toyproject/ui/components/feed/FeedItem.kt
@@ -13,25 +13,29 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.toyproject.ui.theme.Paddings
 
 @Composable
-fun FeedItem() {
+fun FeedItem(
+    username: String,
+    imageUrl: String?,
+    textContent: String,
+    timestamp: String,
+    isLiked: Boolean,
+    onLikedClicked: (Boolean) -> Unit,
+    onCommentClicked: () -> Unit
+) {
     Card(
         shape = MaterialTheme.shapes.large,
         backgroundColor = MaterialTheme.colors.surface,
         elevation = Paddings.none,
         modifier = Modifier.padding(Paddings.medium)
     ) {
-        var isLiked by remember {
-            mutableStateOf(false)
-        }
-        var commentClicked by remember {
-            mutableStateOf(false)
+        var liked by remember {
+            mutableStateOf(isLiked) // 초기 상태를 매개변수에서 가져옴
         }
 
         Column(
@@ -41,33 +45,42 @@ fun FeedItem() {
         ) {
             UserProfileHeader(
                 imageUrl = "",
-                username = "IU",
-                modifier = Modifier.fillMaxWidth()
+                username = username
             )
             Spacer(modifier = Modifier.padding(Paddings.xsmall))
             FeedExpand(
-                textContent = "컨텐츠가 들어갑니다.컨텐츠가 들어갑니다.컨텐츠가 들어갑니다.컨텐츠가 들어갑니다",
-                imageUrl = "dummy"
+                textContent = textContent,
+                imageUrl = imageUrl
             )
-
             Text(
-                text = "xxxx년 xx월 xx일",
+                text = timestamp,
                 modifier = Modifier
                     .padding(horizontal = Paddings.medium)
                     .padding(bottom = Paddings.small)
             )
-
             FeedActions(
-                onCommentClicked = { commentClicked = !commentClicked },
-                onLikedClicked = { isLiked = !isLiked },
-                isLiked = isLiked
+                onCommentClicked = onCommentClicked,
+                onLikedClicked = {
+                    liked = !liked
+                    onLikedClicked(liked)
+                },
+                isLiked = liked
             )
         }
     }
 }
 
+
 @Preview
 @Composable
 fun FeedItemPreview() {
-    FeedItem()
+    FeedItem(
+        username = "IU",
+        imageUrl = "dummy",
+        textContent = "테스트 피드입니다. 여기에는 더 많은 내용들이 들어가게 될 것입니다.",
+        timestamp = "1시간 전",
+        isLiked = false,
+        onLikedClicked = { /* 좋아요 버튼 클릭 시 수행할 로직 */ },
+        onCommentClicked = { /* 댓글 버튼 클릭 시 수행할 로직 */ }
+    )
 }
